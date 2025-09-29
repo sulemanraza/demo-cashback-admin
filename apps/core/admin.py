a7ad23d268b06d60137267f1a17f66c952a6c18d
Suleman Raza <47869006+sulemanraza@users.noreply.github.com>
2025-09-29 11:19:39 +0200

    update

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# site header customization
+admin.site.site_header = "Cashback and Coupons Admin"
+admin.site.site_title = "Cashback and Coupons Admin Portal"
+admin.site.index_title = "Welcome to the Cashback and Coupons Admin Portal"
+admin.site.site_url = None  # Disable the "View Site" link
